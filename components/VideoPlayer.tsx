027d6197ac36c961adf5c4bf9517581256a5d44e
Tran Huynh Tien <coldboykiller@gmail.com>
2026-05-27 11:03:20 +0700

    fix: remove ads in video player (the popup and navigation one)

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -55,6 +55,7 @@ export const VideoPlayer: React.FC<VideoPlayerProps> = ({
                 title={title}
                 className="w-full h-full border-0"
                 allowFullScreen
+                sandbox="allow-scripts allow-forms allow-same-origin"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             />
         </div>
